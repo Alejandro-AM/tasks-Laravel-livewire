371d7225151690607c4ef7506f93159c894d730e
Alejandro-AM <alejandromartinezxp@gmail.com>
2024-11-11 23:51:21 -0600

    Se crearos los modelos y migraciones de task y users

---
+++ app/Models/Task.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Task extends Model
+{
+    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    /**
+     * Get the user that owns the Task
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
