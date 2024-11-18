3ea4d8a43a137b4ea40130e3f644e96fc143cf06
Alejandro-AM <alejandromartinezxp@gmail.com>
2024-11-18 13:10:00 -0600

    Se creo la funcion de compartir una tarea

--- app/Livewire/TaskComponent.php
+++ app/Livewire/TaskComponent.php
@@ -3,26 +3,46 @@
 namespace App\Livewire;
 
 use App\Models\Task;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class TaskComponent extends Component
 {
     public $tasks = [];
+    public $users = [];
     public $id;
     public $title;
     public $description;
+    public $user_id;
+    public $permission;
     public $modal = false;
+    public $modalShare = false;
     public $isUpdating = false;
 
     public function mount()
     {
         $this->tasks = $this->getTasks()->sortByDesc('id');
+        $this->users = User::where('id', '!=', auth()->user()->id)->get();
+    }
+
+    public function renderAllTasks()
+    {
+        $this->tasks = $this->getTasks()->sortByDesc('id');
+    }
+
+    public function render()
+    {
+        return view('livewire.task-component');
     }
 
     public function getTasks()
     {
-        return Task::where('user_id', Auth::user()->id)->get();
+        $user = auth()->user();
+        $myTasks = Task::where('user_id', Auth::user()->id)->get();
+        $mySharedTasks = $user->sharedTasks()->get();
+
+        return $mySharedTasks->merge($myTasks);
     }
 
     private function clearFields()
@@ -31,11 +51,6 @@ class TaskComponent extends Component
         $this->title = '';
         $this->description = '';
         $this->isUpdating = false;
-    }
-
-    public function render()
-    {
-        return view('livewire.task-component');
     }
 
     public function openCreateModal(Task $task = null)
@@ -62,16 +77,22 @@ class TaskComponent extends Component
 
     public function createOrUpdateTask()
     {
-        Task::updateOrCreate(
-            [
-                'id' => $this->id
-            ],
-            [
+        if($this->id)
+        {
+            $task = Task::find($this->id);
+            $task->update([
                 'title' => $this->title,
                 'description' => $this->description,
-                'user_id' => Auth::user()->id,
-            ]
-        );
+            ]);
+        }
+        else
+        {
+            $task = Task::create([
+                'title' => $this->title,
+                'description' => $this->description,
+                'user_id' => $this->id,
+            ]);
+        }
 
         $this->clearFields();
         $this->modal = false;
@@ -84,4 +105,30 @@ class TaskComponent extends Component
         $this->tasks = $this->getTasks()->sortByDesc('id');
     }
 
+    public function openShareModal(Task $task)
+    {
+        $this->modalShare = true;
+        $this->id = $task->id;
+    }
+
+    public function closeShareModal()
+    {
+        $this->modalShare = false;
+    }
+
+    public function shareTask()
+    {
+        $task = Task::find($this->id);
+        $user = User::find($this->user_id);
+        $user->sharedTasks()->attach($task->id, ['permission' => $this->permission]);
+        $this->closeShareModal();
+        $this->tasks = $this->getTasks()->sortByDesc('id');
+    }
+
+    public function taskUnshared(Task $task)
+    {
+        $user = User::find(Auth::user()->id);
+        $user->sharedTasks()->detach($task->id);
+        $this->tasks = $this->getTasks()->sortByDesc('id');
+    }
 }
--- resources/views/livewire/task-component.blade.php
+++ resources/views/livewire/task-component.blade.php
@@ -1,31 +1,37 @@
 <!-- component -->
-<div class="container">
+<div class="container" wire:poll="renderAllTasks">
     <button wire:click="openCreateModal" class="bg-purple-800 text-white px-4 py-2 my-6 rounded-md hover:bg-purple-700">Nuevo</button>
 	<table class="text-center w-full">
 		<thead class="bg-purple-800 flex text-white w-full">
 			<tr class="flex w-full mb-4">
-				<th class="p-4 w-1/3">One</th>
-				<th class="p-4 w-1/3">Two</th>
-				<th class="p-4 w-1/3">Three</th>
+				<th class="p-4 w-1/3">Title</th>
+				<th class="p-4 w-1/3">Description</th>
+				<th class="p-4 w-1/3">Options</th>
 			</tr>
 		</thead>
     <!-- Remove the nasty inline CSS fixed height on production and replace it with a CSS class — this is just for demonstration purposes! -->
-		<tbody class="bg-grey-light flex flex-col items-center justify-between w-full">
+		<tbody class="flex flex-col items-center justify-between w-full">
             @foreach ($tasks as $task)
-            <tr class="flex w-full mb-4">
-				<td class="p-4 w-1/3">{{ $task->title }}</td>
-				<td class="p-4 w-1/3">{{ $task->description }}</td>
-				<td class="p-4 w-1/3">
-                    <button wire:click="openCreateModal({{$task}})" class="bg-yellow-800 text-white px-4 py-2 rounded-md hover:bg-yellow-700">Editar</button>
-                    <button wire:click="deleteTask({{$task}})" class="bg-red-800 text-white px-4 py-2 rounded-md hover:bg-red-500">Borrar</button>
+            <tr class="flex w-full">
+				<td class="p-4 w-1/3 content-center">{{ $task->title }}</td>
+				<td class="p-4 w-1/3 content-center bg-purple-100">{{ $task->description }}</td>
+                @if ((isset($task->pivot) && $task->pivot->permission == 'edit') || auth()->user()->id == $task->user_id)
+                <td class="p-4 w-1/3 content-center">
+                    <button wire:click="openCreateModal({{$task}})" class="bg-yellow-600 text-white px-4 py-2 rounded-md hover:bg-yellow-500">Editar</button>
+                    @if (isset($task->pivot))
+                    <button wire:click="taskUnshared({{$task}})" class="bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-blue-500">Desc</button>
+                    @endif
+                    <button wire:click="openShareModal({{$task}})" class="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-500">Compartir</button>
+                    <button wire:click="deleteTask({{$task}})" wire:confirm="Are you want to delete this task?" class="bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-500">Borrar</button>
                 </td>
+                @endif
 			</tr>
             @endforeach
 		</tbody>
 	</table>
 
     @if ($modal)
-    <!-- component -->
+    <!-- modal form-->
     <div class="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50 py-10">
         <div class="max-h-full w-full max-w-xl overflow-y-auto sm:rounded-2xl bg-white">
         <div class="w-full">
@@ -48,6 +54,46 @@
                     {{isset($id) ? 'Actualizar' : 'Crear'}} tarea
                 </button>
                 <button wire:click="closeCreateModal" class="p-3 bg-white border rounded-full w-full font-semibold">Cancelar</button>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    @endif
+
+    @if ($modalShare)
+    <!-- modal share -->
+    <div class="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50 py-10">
+        <div class="max-h-full w-full max-w-xl overflow-y-auto sm:rounded-2xl bg-white">
+        <div class="w-full">
+            <div class="m-8 my-20 max-w-[400px] mx-auto">
+            <div class="mb-8">
+                <h1 class="mb-4 text-3xl font-extrabold">Compartir tarea</h1>
+                <form>
+                    <div class="mb-4">
+                        <label for="userToShare" class="mb-2 text-sm font-medium text-gray-500">Usuario a compartir</label>
+                        <select wire:model="user_id" name="userToShare" id="userToShare" class="block w-full bg-gray-50 border border-gray-300 text-sm rounded-sm focus:ring-blue-500">
+                            <option value="">Seleccione un usuario</option>
+                            @foreach ($users as $user)
+                                <option value="{{$user->id}}">{{$user->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="mb-4">
+                        <label for="permission" class="mb-2 text-sm font-medium text-gray-500">Permisos</label>
+                        <select wire:model="permission" name="permission" id="permission" class="block w-full bg-gray-50 border border-gray-300 text-sm rounded-sm focus:ring-blue-500">
+                            <option value="">Seleccione el tipo de permiso</option>
+                            <option value="view">Ver</option>
+                            <option value="edit">Editar</option>
+                        </select>
+                    </div>
+                </form>
+            </div>
+            <div class="space-y-4">
+                <button wire:click="shareTask" class="p-3 bg-black rounded-full text-white w-full font-semibold">
+                    Compartir tarea
+                </button>
+                <button wire:click="closeShareModal" class="p-3 bg-white border rounded-full w-full font-semibold">Cancelar</button>
             </div>
             </div>
         </div>
